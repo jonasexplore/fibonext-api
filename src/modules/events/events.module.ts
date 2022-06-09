6be2adf0c4917625328d88b8c3aa27cd8bdd002e
Jonas <jonasexplore@gmail.com>
2022-06-08 21:50:46 -0300

    chore: instanced cache module and config module

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -1,7 +1,19 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import * as redisStore from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      user: process.env.REDIS_USER,
+      password: process.env.REDIS_PASSWORD,
+    }),
+  ],
   providers: [EventsGateway],
 })
 export class EventsModule {}
