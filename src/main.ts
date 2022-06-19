1996d0e43aff3fff351a7b06f991dba0d8d62e72
Jonas <jonasexplore@gmail.com>
2022-06-19 10:16:55 -0300

    chore: getting port from envs

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'verbose', 'log'],
   });
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
